09342cdc96f6e786fb5426dedd8ee984abe261e5
Ben Yip <ben_yip@126.com>
2023-10-13 01:28:40 +0800

    localStragy validate function

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,9 +1,16 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  UnauthorizedException,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Strategy } from 'passport-local';
 import { User } from 'src/db/entities/User';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -14,5 +21,29 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super();
   }
 
-  async validate(username: string, password: string): Promise<any> {}
+  async validate(phoneOrEmail: string, password: string): Promise<any> {
+    try {
+      const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      const phoneReg = /^1[3456789]\d{9}$/;
+      if (!emailReg.test(phoneOrEmail) && !phoneReg.test(phoneOrEmail))
+        throw new UnauthorizedException(
+          'Please input correct mobile phone number or email address.',
+        );
+      const user = await this.userRepository.findOne({
+        where: [{ email: phoneOrEmail }, { mobilePhone: phoneOrEmail }],
+      });
+
+      if (!user) {
+        this.logger.debug(`user is NOT Found.`);
+        throw new UnauthorizedException('User is NOT FOUND.');
+      }
+
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) throw new UnauthorizedException('Invalid password.');
+
+      return user;
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
--- src/db/entities/User.ts
+++ src/db/entities/User.ts
@@ -2,15 +2,20 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity({ name: 'users' })
 export class User {
+  static tableName = 'users';
+
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  name: string;
+  @Column({ name: 'user_name' })
+  userName: string;
 
   @Column()
   password: string;
 
   @Column({ unique: true })
   email: string;
+
+  @Column({ unique: true, name: 'mobile_phone' })
+  mobilePhone: string;
 }
